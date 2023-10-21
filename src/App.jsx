001304410af1e4e37e9a8d1d6dd6526649bf2efe
Abu Zaid <127389591+Abuzaid98@users.noreply.github.com>
2023-10-21 15:54:23 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,29 @@ const App = () => {
     const [toggleBtn, setToggleBtn] = useState(true)
     const [getId, setGetid] = useState(null)
     const inputRef = useRef(null)
+    
 
-    const handleChange = (e) => {
-        setInputVal(e.target.value)
-        setFocus(e.target.focus())
-    }
+
 
     const handleAdd = () => {
-        if(inputVal.length > 0){   
-        setTodos(prevTodos => [inputVal, ...prevTodos])
-        setInputVal('')
+        if (inputVal.length > 0) {
+            setTodos((prevTodos) => {
+                return [{id: crypto.randomUUID(), title:inputVal, completed:false}, ...prevTodos]
+            })
+            setInputVal('')
         }
+    }
+
+    const toggleCheck = (id, completed)=>{
+        setTodos(prev => {
+            return prev.map(todo =>{
+                if(todo.id === id){
+                    return {...todo, completed}
+
+                }
+                return todo
+            })
+        })
     }
 
     const handleDel = (i) => {
@@ -36,34 +48,36 @@ const App = () => {
     }
 
     const handleUpdate = () => {
-        if(inputVal.length > 0){            
+        if (inputVal.length > 0) {
             const rcd = [...todos]
-            rcd[getId]= inputVal
+            rcd[getId].title = inputVal
             setTodos(rcd)
             setToggleBtn(true)
             setInputVal('')
         }
     }
 
-    const handleSubmit = (e)=>{
-        if(e.key == 'Enter' && inputVal.length > 0){
-            if(toggleBtn){
+    const handleSubmit = (e) => {
+        if (e.key == 'Enter' && inputVal.length > 0) {
+            if (toggleBtn) {
                 handleAdd()
-            }else{
+            } else {
                 handleUpdate()
             }
         }
     }
 
-    useEffect(()=>{
-        localStorage.setItem('todos',JSON.stringify(todos))
-    },[todos])
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
 
     return (
         <div className='container'>
-            <h1 className='heading'>To Do APP</h1>
+            <h1 className='heading'>To Do App</h1>
             <div className="inputField">
-                <input type="text" ref={inputRef} onKeyUp={handleSubmit} onChange={handleChange} placeholder='To-do...' value={inputVal} />
+                <input type="text" ref={inputRef} onKeyUp={handleSubmit}
+                    onChange={(e) => setInputVal(e.target.value)} placeholder='To-do...' value={inputVal} />
+
                 {
                     toggleBtn ? <button onClick={handleAdd}>Add</button> : <button onClick={handleUpdate}>Update</button>
                 }
@@ -73,9 +87,12 @@ const App = () => {
                 todos.map((todo, i) => {
                     return (
                         <div className='todos' key={i}>
-                            <p className='text'>{todo}</p>
+                            <div className='allText'>
+                                <input type="checkbox" checked={todo.completed} onChange={(e)=> toggleCheck(todo.id, e.target.checked)} />
+                                <p className={`text${todo.completed ? ' checked' : ''}`}>{todo.title}</p>
+                            </div>
                             <div className="action">
-                                <span className='edit' onClick={() => handleEdit(todo, i)}><i className="fa fa-edit"></i></span>
+                                <span className='edit' onClick={() => handleEdit(todo.title, i)}><i className="fa fa-edit"></i></span>
                                 <span className='delete' onClick={() => handleDel(i)}><i className="fa fa-trash-o"></i></span>
                             </div>
 
